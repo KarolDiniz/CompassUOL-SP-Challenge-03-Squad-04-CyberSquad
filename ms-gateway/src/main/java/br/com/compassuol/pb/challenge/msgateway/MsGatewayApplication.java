b2145fe85bbf1fbb9bf04530c3a89407335e5d3c
KarolDiniz <dinizkaroline923@gmail.com>
2023-07-04 19:18:59 -0300

    API-GATEWAY: Creating a api cloud gateway

---
+++ ms-gateway/src/main/java/br/com/compassuol/pb/challenge/msgateway/MsGatewayApplication.java
@@ -0,0 +1,25 @@
+package br.com.compassuol.pb.challenge.msgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r.path("/categories/**").uri("lb://msproducts"))
+				.route(r -> r.path("/products/**").uri("lb://msproducts"))
+				.build();
+	}
+}
---
+++ ms-gateway/src/test/java/br/com/compassuol/pb/challenge/msgateway/MsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compassuol.pb.challenge.msgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
