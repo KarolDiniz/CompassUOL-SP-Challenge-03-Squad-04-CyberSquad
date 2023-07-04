4a6e70794fca6d8887d6825fd04a22e6684e4c8a
KarolDiniz <dinizkaroline923@gmail.com>
2023-07-03 23:59:41 -0300

    MS-PRODUCTS: Modifying the application

---
+++ ms-products/src/main/java/br/com/compassuol/pb/challenge/msproducts/MsProductsApplication.java
@@ -0,0 +1,15 @@
+package br.com.compassuol.pb.challenge.msproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+//@EnableEurekaClient
+public class MsProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProductsApplication.class, args);
+	}
+
+}
