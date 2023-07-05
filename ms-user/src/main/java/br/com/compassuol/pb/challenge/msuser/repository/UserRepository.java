ff54908d69d3e79d0aff1a2da0b885e12006fd19
KarolDiniz <dinizkaroline923@gmail.com>
2023-07-05 14:19:49 -0300

    MS-USERS: Creating Repositories

---
+++ ms-user/src/main/java/br/com/compassuol/pb/challenge/msuser/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package br.com.compassuol.pb.challenge.msuser.repository;
+import br.com.compassuol.pb.challenge.msuser.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
