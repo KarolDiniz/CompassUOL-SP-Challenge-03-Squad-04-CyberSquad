3f515d427a26cd641e0ce96c93ca29920c0d863d
KarolDiniz <dinizkaroline923@gmail.com>
2023-07-05 14:18:58 -0300

    MS-USERS: Creating UserController

---
+++ ms-user/src/main/java/br/com/compassuol/pb/challenge/msuser/controller/UserController.java
@@ -0,0 +1,37 @@
+package br.com.compassuol.pb.challenge.msuser.controller;
+
+import br.com.compassuol.pb.challenge.msuser.service.UserService;
+import br.com.compassuol.pb.challenge.msuser.entities.User;
+import br.com.compassuol.pb.challenge.msuser.dto.UserDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@Valid @RequestBody UserDTO userDTO) {
+        User createdUser = userService.createUser(userDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+//    @PutMapping("/{id}")
+//    public ResponseEntity<User> updateUser(@PathVariable Long id, @Valid @RequestBody UserDTO userDTO) {
+//        User updatedUser = userService.updateUser(id, userDTO);
+//        return ResponseEntity.ok(updatedUser);
+//    }
+}
