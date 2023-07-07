940c6d3a0191dfe90c6c13a0fd513eda4a08d639
KarolDiniz <dinizkaroline923@gmail.com>
2023-07-07 17:02:47 -0300

    MS-NOTIFICATION: Modifying msUserApplication

--- ms-user/src/main/java/br/com/compassuol/pb/challenge/msuser/MsUserApplication.java
+++ ms-user/src/main/java/br/com/compassuol/pb/challenge/msuser/MsUserApplication.java
@@ -1,10 +1,11 @@
 package br.com.compassuol.pb.challenge.msuser;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+@EnableRabbit
 public class MsUserApplication {
 
 	public static void main(String[] args) {
