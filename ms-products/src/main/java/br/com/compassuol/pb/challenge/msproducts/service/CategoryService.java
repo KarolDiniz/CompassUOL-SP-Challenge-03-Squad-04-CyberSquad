0c46cf4151601a387e52efbdfc0119a34eb27c90
KarolDiniz <dinizkaroline923@gmail.com>
2023-07-09 20:08:22 -0300

    MS-PRODUCTS: Adding logs

--- ms-products/src/main/java/br/com/compassuol/pb/challenge/msproducts/service/CategoryService.java
+++ ms-products/src/main/java/br/com/compassuol/pb/challenge/msproducts/service/CategoryService.java
@@ -4,6 +4,7 @@ import br.com.compassuol.pb.challenge.msproducts.domain.Category;
 import br.com.compassuol.pb.challenge.msproducts.dto.CategoryDTO;
 import br.com.compassuol.pb.challenge.msproducts.repository.CategoryRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
+@Slf4j
 @AllArgsConstructor
 @Transactional
 public class CategoryService {
@@ -18,12 +20,14 @@ public class CategoryService {
     private final CategoryRepository categoryRepository;
 
     public Category createCategory(CategoryDTO categoryDTO) {
+        log.info("Creating a new category: {}", categoryDTO);
         Category category = new Category();
         BeanUtils.copyProperties(categoryDTO, category);
         return categoryRepository.save(category);
     }
 
     public List<Category> getAllCategories() {
+        log.info("Fetching all categories");
         return categoryRepository.findAll();
     }
 }
