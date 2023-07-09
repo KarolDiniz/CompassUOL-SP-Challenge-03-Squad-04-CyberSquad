f9905f7bd23f7f173af2f8805daec2499ffe849e
KarolDiniz <dinizkaroline923@gmail.com>
2023-07-09 13:34:46 -0300

    MS-PRODUCTS: Modifying dto

--- ms-products/src/main/java/br/com/compassuol/pb/challenge/msproducts/dto/CategoryDTO.java
+++ ms-products/src/main/java/br/com/compassuol/pb/challenge/msproducts/dto/CategoryDTO.java
@@ -1,8 +1,12 @@
 package br.com.compassuol.pb.challenge.msproducts.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryDTO {
     private Long id;
     private String name;
