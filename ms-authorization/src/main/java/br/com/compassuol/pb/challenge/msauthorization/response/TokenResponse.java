ddb91aa52aeafd96a2e0fb67d2912c4888d03698
KarolDiniz <dinizkaroline923@gmail.com>
2023-07-10 16:28:55 -0300

    MS-AUTH: Create tokenResponse

---
+++ ms-authorization/src/main/java/br/com/compassuol/pb/challenge/msauthorization/response/TokenResponse.java
@@ -0,0 +1,13 @@
+package br.com.compassuol.pb.challenge.msauthorization.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class TokenResponse {
+    private String token;
+
+}
