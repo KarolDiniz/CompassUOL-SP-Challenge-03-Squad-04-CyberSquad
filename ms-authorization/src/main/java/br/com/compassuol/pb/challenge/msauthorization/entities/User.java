f1ce11cc8f0b4730982f39ba5d5dead7ff8dcac7
KarolDiniz <dinizkaroline923@gmail.com>
2023-07-10 10:00:20 -0300

    MS-AUTH: Creating User

---
+++ ms-authorization/src/main/java/br/com/compassuol/pb/challenge/msauthorization/entities/User.java
@@ -0,0 +1,18 @@
+package br.com.compassuol.pb.challenge.msauthorization.entities;
+
+import java.util.List;
+public class User {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    private List<Role> roles;
+
+}
